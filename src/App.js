2a16300e3010837900e51312d5701cdf5e7c4e96
Kwesi Larbi <KwesiiiLarb@gmail.com>
2021-01-12 22:45:58 -0500

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
+import { Component } from 'react';
+import Clock from './Components/Clock';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+let time = new Date().toLocaleString();
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header id="header">
+						<div class="logo">
+							<span class="icon fa-clock"></span>
+						</div>
+						<div class="content">
+							<div class="inner">
+								<h1>React Clock</h1>
+								<p>A fully responsive site template designed by <a href="https://html5up.net">HTML5 UP</a> and released<br />
+								for free under the <a href="https://html5up.net/license">Creative Commons</a> license.</p>
+                <p className="App-intro">
+                  Hi {this.props.name}
+                </p>
+                <Clock />
+							</div>
+						</div>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
